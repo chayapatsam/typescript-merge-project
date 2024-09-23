e2c6efcfa478e4f1ff3b645e4e3d4dbc7d0adf07
[chayapat samapak] <[chayapat.sam@dome.tu.ac.th]>
2024-09-24 00:21:36 +0700

    config + code

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+// jest.config.ts
+
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
